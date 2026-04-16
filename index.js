4acf3dc9ea03d2ed5ede0e5dd871f382dc3da395
Bryton <bryton387m@gmail.com>
2026-04-16 15:01:29 +0300

    done

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 
+function combineUsers(...arrays) {
+  const users = arrays.flat();
+  const merge_date = Date.today().toString("M/d/yyyy");
+  return { users, merge_date };
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  combineUsers
 };
